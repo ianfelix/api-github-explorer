166f64b5c862557cbbebd42f0e5f6638c9c6599c
ianfelix <ianfelix030@gmail.com>
2021-04-23 12:16:02 -0300

    feat: add chakra ui

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,14 +15,8 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
         <body>
+          <Head />
           <Main />
           <NextScript />
         </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Github!</h1>
-    </div>
+    <>
+      <Head>
+        <title>GitHub API</title>
+      </Head>
+
+      <div>
+        <h1>Hello Github!</h1>
+      </div>
+    </>
   );
 }
