bd5141883f043c3714a184b7cfdb067fc8f1648e
ianfelix <ianfelix030@gmail.com>
2021-04-24 23:16:34 -0300

    feat: add timeout and auth

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: 'https://api.github.com/',
+  headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}` },
+  timeout: 1000,
 });
