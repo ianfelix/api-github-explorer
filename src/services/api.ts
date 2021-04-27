be0bd64273c01ab60453007554bad8ce53075150
ianfelix <ianfelix030@gmail.com>
2021-04-26 23:01:00 -0300

    feat: remove timeout

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,4 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://api.github.com/',
   headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}` },
-  timeout: 1000,
 });
